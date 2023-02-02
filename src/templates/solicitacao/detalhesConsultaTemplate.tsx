d287b32d779f97ed62cbbd213c0d10862c817780
Felipe Santiago Gama <felipes4ntiago@hotmail.com>
2023-02-01 21:02:01 -0300

    Criação da tela de Listagem de Consultas, visão do médico

---
+++ src/templates/solicitacao/detalhesConsultaTemplate.tsx
@@ -0,0 +1,50 @@
+import Layout from "@/components/layout/Layout"
+import styles from "../../styles/visualizacaoDeSolicitacao.module.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import { SeyfertConsultaRequestService } from "@/services/SeyfertConsultaRequestServicec"
+import { VisualizarConsultaResponse } from "@/types/consulta/VisualizarConsultaResponse"
+import { AxiosResponse } from "axios"
+
+type props = {
+    id: string
+}
+const DetalhesConsultaTemplate = ({id}:props) => {
+    const [consultaResponse, setConsultaResponse] = useState<VisualizarConsultaResponse>();
+
+    useEffect(() => {
+        if(!id) return;
+
+        SeyfertConsultaRequestService.visualizarConsulta(id).then(({ data }: AxiosResponse<VisualizarConsultaResponse>) => {
+            console.log(data)
+            setConsultaResponse(data);
+        }).catch(( {response} ) => {
+            alert("Deu problema")
+        })
+    }, [id]);
+    return(
+        <Layout titleHeader="Detalhamento da solicitação de consulta">
+        <div>
+            <h1>Consulta</h1>
+            <ul>
+                <li>Nome do paciente: Járdesson Ribeiro</li>
+                <li>Data da consulta:  10/10/2010</li>
+                <li>Horário do início:  10:00</li>
+                <li>Horário do fim: 11:00</li>
+                <li>Descrição da consulta: <p>Desde o dia 13/05 desse ano vim sentindo fortes dores nas costas, e a situação vem piorando desde que...</p></li>
+                <li>Valor da consulta:  500 Reais</li>
+
+
+            </ul>
+            {/* onClick={mudarPagina} */}
+            <button  name="button">Criar proposta de consulta</button>
+        </div>
+        </Layout>
+
+    )
+}
+
+
+export default DetalhesConsultaTemplate
---
+++ src/templates/solicitacao/index.tsx
@@ -0,0 +1,49 @@
+import Layout from "@/components/layout/Layout"
+import styles from "../../styles/visualizacaoDeSolicitacao.module.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Link from "next/link"
+import { useRouter } from "next/router"
+
+const VisualizacaoDeSolicitacaoTemplate = () => {
+    const route = useRouter()
+
+     function mudarPagina(){
+        route.push("../../solicitacao/detalhesSolicitacao/idConsulta");
+
+     }
+    return(
+        <Layout titleHeader="Listagem de Solicitações de consulta">
+        <div >
+            <table>
+                <thead>
+                <tr>
+                    <th className={styles.elementoTabela}>Paciente</th>
+                    <th className={styles.elementoTabela}>Data</th>
+                    <th className={styles.elementoTabela}>Horário Inicial</th>
+                    <th className={styles.elementoTabela}>Horário Final</th>
+                    <th  className={styles.elementoTabela}>Descrição da necessidade</th>
+                    <th  className={styles.elementoTabela}>Excluir</th>
+                    <th  className={styles.elementoTabela}>Visualizar</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td className={styles.elementoTabela}>Felipe Santiago Gama</td>
+                    <td  className={styles.elementoTabela}>12/08/2002</td>
+                    <td className={styles.elementoTabela}>10:00</td>
+                    <td className={styles.elementoTabela}>12:00</td>
+                    <td className={styles.elementoTabela}>Covid</td>
+                    <td className={styles.elementoTabela}><button onClick={()=>alert("clicou em excluir")} name="button">Excluir</button></td>
+                    <td className={styles.elementoTabela}><button onClick={mudarPagina} name="button">Visualizar</button></td>
+                </tr>
+                </tbody>
+            </table>
+
+  </div>
+  </Layout>
+
+    )
+}
+
+
+export default VisualizacaoDeSolicitacaoTemplate
