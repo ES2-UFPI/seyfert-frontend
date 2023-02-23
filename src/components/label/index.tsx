42239bd95a223b65dca0bd673f4df8d6c1f02eda
Járdesson Ribeiro <jardesson.santos@maida.health>
2023-02-23 08:31:04 -0300

    Jardesson-Ajustes_Front Remoção de alguns estilos do css Remoção de imports não usados

--- src/components/label/index.tsx
+++ src/components/label/index.tsx
@@ -5,28 +5,16 @@ type TypeLabel = "CLEAN" | "AREA"
 
 type LabelProps = {
   title: string
-  type: TypeLabel
   children: React.ReactNode
 }
 
-export default function Label({title, type, children}: LabelProps) {
-  if(type === "CLEAN"){
-    return (
-      <div className={styles.container_clean}>
-          <span className={styles.title}>{title}</span>
-          <div className={styles.content_container_clean}>
-              <p className={styles.content}>{children}</p>
-          </div>
-      </div>
-    )
-  }else {
-    return (
-      <div className={styles.container_area}>
-          <span className={styles.title}>{title}</span>
-          <div className={styles.content_container_area}>
-              <p className={styles.content}>{children}</p>
-          </div>
-      </div>
-    )
-  }
+export default function Label({title, children}: LabelProps) {
+  return (
+    <div className={styles.container}>
+        <span className={styles.title}>{title}</span>
+        <div className={styles.content_container}>
+            <p className={styles.content}>{children}</p>
+        </div>
+    </div>
+  )
 }
--- src/templates/layout/Layout.tsx
+++ src/templates/layout/Layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from './styles.module.css'
-import Header from '../header'
 
 type LayoutProps = {
   title?: string
@@ -13,12 +12,9 @@ export default function Layout({titleHeader, title, children}: LayoutProps) {
   return (
     <>
       <title>{title != undefined ? title : "Página"}</title>
-      <Header/>
       <div className={styles.grid}>
         <div className={styles.header}>{titleHeader}</div>
-        
         <div className={styles.content}>{children}</div>
-        <div className={styles.siderigth}>Aqui tem um siderigth</div>
       </div>
     </>
   )
