f8190f9c18a176fdf447f68b6b3943ffec0b09a7
Felipe Santiago Gama <felipes4ntiago@hotmail.com>
2023-02-28 08:39:33 -0300

    criando página da tela de cancelar consultas

---
+++ src/pages/consulta/medicoCancelaConsulta.tsx
@@ -0,0 +1,19 @@
+/*Essa tela deve ser uma continuação do fluxo da tela de Listar as consultas(médico), de forma que tenha uma forma de um médico ser
+redirecionado a essa página para cancelar uma consulta, desde que elá não tenha passado do prazo.*/
+
+export default function medicoCancelaConsulta(){
+    
+
+    return(
+    <>
+    <table>
+        
+
+
+    </table>
+    </>
+    )
+
+
+
+}
