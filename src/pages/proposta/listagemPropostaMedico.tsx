5a5922642479703206557c7da93bfdbc95e9151d
Felipe Santiago Gama <felipes4ntiago@hotmail.com>
2023-03-01 22:27:07 -0300

    criando página para listagem de proposta de médico #10

---
+++ src/pages/proposta/listagemPropostaMedico.tsx
@@ -0,0 +1,9 @@
+/*Uma proposta consite em:  Valor da consulta, Data da consulta, hora inicial, hora final , medico, solicitação, situação proposta */
+
+export default function listagemPropostaMedico(){
+
+    return(
+        <>
+        </>
+    )
+}
