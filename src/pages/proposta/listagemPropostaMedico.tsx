c13a64bd8d4d4fc20c9529aa9a72217dd29de5ad
Felipe Santiago Gama <felipes4ntiago@hotmail.com>
2023-03-01 22:41:38 -0300

    tentando resolver erro da build #18

--- src/pages/proposta/listagemPropostaMedico.tsx
+++ src/pages/proposta/listagemPropostaMedico.tsx
@@ -4,6 +4,24 @@ export default function listagemPropostaMedico(){
 
     return(
         <>
+        <h1>Listagem de propostas</h1>
+
+        <table>
+        <thead>
+          <tr>
+            <th>Valor da consulta</th>
+            <th>Data da consulta</th>
+            <th>Horário Inicial</th>
+            <th>Horário Final</th>
+            <th>Médico</th>
+            <th>Visualizar solicitação</th>
+            <th>Visualizar</th>
+          </tr>
+        </thead>
+            <tbody>
+
+            </tbody>
+        </table>
         </>
     )
 }
