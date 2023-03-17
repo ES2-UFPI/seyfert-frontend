1b1d23be2952d989d059bdb9fc1f7a11402316d6
Járdesson Ribeiro <jardesson.santos@maida.health>
2023-03-17 17:28:08 -0300

    Jardesson_Criacao_Tela_Home_Refatoracao Refatoração do código Adição do globals.css Criação de Tela home

--- src/templates/home/index.tsx
+++ src/templates/home/index.tsx
@@ -1,25 +1,3 @@
 import Link from 'next/link';
 import styles from './styles.module.css';
 
-const Home = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.header}>
-                <p>Header de Home</p>
-            </div>
-            <div className={styles.container_content}>
-                <p className={styles.name_seyfert}>Bem -Vindo(as) ao Seyfert</p>
-                <div className={styles.container_links}>
-                    <div className={styles.links}>
-                        <span><Link className={styles.link} href="/">Login</Link></span>
-                    </div>
-                    <div className={styles.links}>
-                        <span><Link className={styles.link} href="/">Cadastre-se</Link></span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
-
-export default Home;
