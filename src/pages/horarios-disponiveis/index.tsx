4e1e769e1a2c3e6e224bda47c34828be909b4a7d
Járdesson Ribeiro <jardesson.santos@maida.health>
2023-03-21 08:52:32 -0300

    Jardesson_Criacao_Tela_Home_Refatoracao Ajustes em tela de horário disponíveis

--- src/pages/horarios-disponiveis/index.tsx
+++ src/pages/horarios-disponiveis/index.tsx
@@ -1,120 +1,123 @@
 import Layout from "@/layout/Layout";
-import styles from "./horariosDisponiveis.module.css";
+import styles from "./styles.module.css";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import ButtonComponent from "@/components/button";
 
 
 const HorariosDisponiveis = () => {
   const route = useRouter();
 
+  function openAgendarConsulta(uuidHorario: string){
+    route.push(`/agendar-consulta/${uuidHorario}`)
+  }
+
   return (
     <Layout titleHeader="Horarios Disponiveis">
-      <div>
-        <table className={styles.tabela}>
+    <div className={styles.container}>
+      <div className={styles.container_search}>
+        <div className={styles.inputs_search}>
+          <input type="text" placeholder="Busque por um(a) médico(a)"/>
+          <input
+            className={styles.input}
+            id="diasdasemana"
+            list="dias"
+            // onChange={(e) => alert(e.target.value)}
+          />
+          <datalist id="dias">
+            <option value="SEGUNDA" />
+            <option value="TERCA" />
+            <option value="QUARTA" />
+            <option value="QUINTA" />
+            <option value="SEXTA" />
+            <option value="SABADO" />
+            <option value="DOMINGO" />
+          </datalist>
+          <input
+            className={styles.input}
+            id="especi"
+            list="especialidade"
+            // onChange={(e) => alert(e.target.value)}
+            required
+          />
+          <datalist id="especialidade">
+            <option value="Clínico Geral" />
+            <option value="Cardiologista" />
+            <option value="Oftalmologista" />
+            <option value="Ortopedista" />
+            <option value="Neurologista" />
+          </datalist>
+          <input type="time" placeholder="Horário inicial ex: 10:00"/>
+          <input type="time" placeholder="Horário final ex: 11:00"/>
+        </div>
+        <div className={styles.area_button}>
+            <ButtonComponent color="rgb(29, 66, 230)" childrenColor="rgb(255, 255, 255)">Confirmar</ButtonComponent>
+        </div>
+      </div>
+      <div className={styles.container_list}>
+      <table className={styles.tabela}>
+          <thead className={styles.table_top}>
+            <tr>
+              <th className={styles.theader}>
+                <h4>Médico</h4>
+              </th>
+              <th className={styles.theader}>
+                <h4>Especialidade</h4>
+              </th>
+              <th className={styles.theader}>
+                <h4>Dia da semana</h4>
+              </th>
+              <th className={styles.theader}>
+                <h4>Horários</h4>
+              </th>
+              <th className={styles.theader}>
+                <h4>Valor</h4>
+              </th>
+              <th className={styles.theader}>
+                <h4>Ações</h4>
+              </th>
+            </tr>
+          </thead>
           <tbody>
-            <tr>
-              <td colSpan={6}>
-                <center>
-                  <p className={styles.data}>08/02/2023</p>
-                </center>
+            <tr className={styles.tbody_line}>
+              <td className={styles.tdcontent}>João da Silva Silveira</td>
+              <td className={styles.tdcontent}>Oftalmologista</td>
+              <td className={styles.tdcontent}>SEGUNDA</td>
+              <td className={styles.tdcontent}>11:20 até 12:00</td>
+              <td className={styles.tdcontent}>R$ 200,00</td>
+              <td className={styles.tdcontent_button}>
+                <button onClick={e => openAgendarConsulta("b057b0a6-9f29-442f-bd0b-e51277d9fa60")} className={styles.botao}>Marcar Consulta</button>
               </td>
             </tr>
-            <tr>
-              <td width={"25%"} className={styles.coluna}>
-                <h4>Médico</h4>
-              </td>
-              <td width={"13%"} className={styles.coluna}>
-                <h4>Especialidade</h4>
-              </td>
-              <td width={"13%"} className={styles.coluna}>
-                <h4>Horário</h4>
-              </td>
-              <td width={"13%"} className={styles.coluna}>
-                <h4>Valor</h4>
-              </td>
-              <td width={"26%"} className={styles.coluna}>
-                <h4>Diagnóstico</h4>
-              </td>
-            </tr>
-            <tr>
-              <td className={styles.coluna}>João da Silva Silveira</td>
-              <td className={styles.coluna}>Oftalmologista</td>
-              <td className={styles.coluna}>11:20</td>
-              <td className={styles.coluna}>Retorno</td>
-              <td className={styles.coluna}>
-                O paciente está continua com um cisco no olho
-              </td>
-              <td className={styles.coluna}>
-                <button className={styles.botao}>Visualizar Consulta</button>
-              </td>
-            </tr>
-            <tr>
-              <td className={styles.coluna}>Mariana Santana dos Santos</td>
-              <td className={styles.coluna}>Psicóloga</td>
-              <td className={styles.coluna}>09:00</td>
-              <td className={styles.coluna}>R$200,00</td>
-              <td className={styles.coluna}>O paciente parece ser paranóico</td>
-              <td className={styles.coluna}>
-                <button className={styles.botao}>Visualizar Consulta</button>
-              </td>
-            </tr>
-            <tr>
-              <td className={styles.coluna}>Joaquim de Aquino</td>
-              <td className={styles.coluna}>Cardiologista</td>
-              <td className={styles.coluna}>18:00</td>
-              <td className={styles.coluna}>R$120,00</td>
-              <td className={styles.coluna}>
-                O paciente está com o coração em bom estado
-              </td>
-              <td className={styles.coluna}>
-                <button className={styles.botao}>Visualizar Consulta</button>
-              </td>
-            </tr>
-            <tr>
-              <td colSpan={6}>
-                <center>
-                  <p className={styles.data}>01/02/2023</p>
-                </center>
-              </td>
-            </tr>
-            <tr>
-              <td width={"25%"} className={styles.coluna}>
-                <h4>Médico</h4>
-              </td>
-              <td width={"13%"} className={styles.coluna}>
-                <h4>Especialidade</h4>
-              </td>
-              <td width={"13%"} className={styles.coluna}>
-                <h4>Horário</h4>
-              </td>
-              <td width={"13%"} className={styles.coluna}>
-                <h4>Valor</h4>
-              </td>
-              <td width={"26%"} className={styles.coluna}>
-                <h4>Diagnóstico</h4>
-              </td>
-            </tr>
-            <tr>
-              <td className={styles.coluna}>João da Silva Silveira</td>
-              <td className={styles.coluna}>Oftalmologista</td>
-              <td className={styles.coluna}>16:40</td>
-              <td className={styles.coluna}>R$150,00</td>
-              <td className={styles.coluna}>
-                O paciente está com um cisco no olho
-              </td>
-              <td className={styles.coluna}>
-                <button className={styles.botao}>Visualizar Consulta</button>
+            <tr className={styles.tbody_line}>
+              <td className={styles.tdcontent}>João da Silva Silveira</td>
+              <td className={styles.tdcontent}>Oftalmologista</td>
+              <td className={styles.tdcontent}>SEGUNDA</td>
+              <td className={styles.tdcontent}>11:20 até 12:00</td>
+              <td className={styles.tdcontent}>R$ 200,00</td>
+              <td className={styles.tdcontent_button}>
+                <button onClick={e => openAgendarConsulta("b057b0a6-9f29-442f-bd0b-e51277d9fa60")} className={styles.botao}>Marcar Consulta</button>
               </td>
             </tr>
           </tbody>
         </table>
+        <div className={styles.container_pagination}>
+            <div className={styles.pagination}>
+              <span className={styles.pagination_element}>1</span>
+              <span className={styles.pagination_element}>2</span>
+              <span className={styles.pagination_element}>3</span>
+              <span className={styles.pagination_element}>4</span>
+              <span className={styles.pagination_element}>5</span>
+            </div>
+            <div className={styles.mounts}>
+              <span>2 de 10</span>
+            </div>
+
+        </div>
       </div>
+    </div>
     </Layout>
   );
-};
-
-HorariosDisponiveis.getLayout = function getLayout(page: JSX.Element) {
-  return page;
 };
 
 export default HorariosDisponiveis;
